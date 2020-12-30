fa964ff86f5e9469956f86ae3b50a1a685548101
iaabo <insbarroso@gmail.com>
2020-12-30 21:30:48 +0000

    delete bookdetails component + route

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import MenuBookworm from "./components/Bookworm/MenuBookworm";
 import MovieList0 from "./components/Cinephile/MovieList0";
 import MovieList50 from "./components/Cinephile/MovieList50";
 import MovieList200 from "./components/Cinephile/MovieList200";
-import BookDetails from "./components/Bookworm/BookDetails";
 import BookList0 from "./components/Bookworm/BookList0";
 import BookList50 from "./components/Bookworm/BookList50";
 import BookList200 from "./components/Bookworm/BookList200";
@@ -26,7 +25,6 @@ function App() {
         <Route path="/movies-1" component={MovieList0} />
         <Route path="/movies-2" component={MovieList50} />
         <Route path="/movies-3" component={MovieList200} />
-        <Route path="/book/:id" component={BookDetails} />
         <Route path="/books-1" component={BookList0} />
         <Route path="/books-2" component={BookList50} />
         <Route path="/books-3" component={BookList200} />
