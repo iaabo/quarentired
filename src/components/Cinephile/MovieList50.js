db0d81a037dda862effdf938ae23a5eee8f88f4d
iaabo <insbarroso@gmail.com>
2020-12-30 23:47:57 +0000

    change the movies when fetching the API

--- src/components/Cinephile/MovieList50.js
+++ src/components/Cinephile/MovieList50.js
@@ -6,7 +6,7 @@ const MovieList50 = () => {
   const [movies50, setMovies50] = useState([]);
 
   const getMovieRequest50 = async () => {
-    const url = "http://www.omdbapi.com/?s=fast_furious&type=movie&apikey=68259e92";
+    const url = "http://www.omdbapi.com/?s=superman&type=movie&apikey=68259e92";
 
     const response = await fetch(url);
     const responseJson = await response.json();
@@ -21,9 +21,7 @@ const MovieList50 = () => {
 
   return (
     <div>
-          <h1 className="recomends">
-        Quarentired recommends a marathon of ...
-      </h1>
+      <h1 className="recomends">Quarentired recommends a marathon of ...</h1>
       <Movie50 movies50={movies50} />
       <Link to="/menu-cinema">Return</Link>
     </div>
